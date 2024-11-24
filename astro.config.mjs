e21a1e39974691456ed81b3370d5cfd093cf1fca
Karim Daghari <50710559+karimdaghari@users.noreply.github.com>
2024-11-24 17:21:29 +0100

    Enable linter in Biome configuration, remove outdated blog examples, update footer copyright, and enhance 404 page with random memes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,17 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
-  markdown: {
-    shikiConfig: {
-      theme: "css-variables",
-    },
-  },
+	site: "https://karimdaghari.com",
+	integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+	markdown: {
+		shikiConfig: {
+			theme: "css-variables",
+		},
+	},
+	redirects: {
+		"/x": "https://bsky.app/profile/karimdaghari.com",
+		"/bsky": "https://bsky.app/profile/karimdaghari.com",
+		"/linkedin": "https://www.linkedin.com/in/karimdaghari",
+		"/github": "https://www.github.com/karimdaghari",
+	},
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,43 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+	TITLE: "Karim Daghari",
+	DESCRIPTION: "KD's little corner of the internet.",
+	EMAIL: "hi@karimdaghari.com",
+	NUM_POSTS_ON_HOMEPAGE: 3,
+	NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+	TITLE: "Home",
+	DESCRIPTION: "Welcome to my little corner of the internet.",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+	TITLE: "Blog",
+	DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+	TITLE: "Projects",
+	DESCRIPTION:
+		"A collection of my projects, some with links to repositories and live demos.",
 };
 
 export const SOCIALS: Socials = [
-  {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
-  },
-  {
-    NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
-  },
-  {
-    NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
-  },
+	{
+		NAME: "Bsky",
+		HREF: "https://bsky.app/profile/karimdaghari.com",
+	},
+	{
+		NAME: "GitHub",
+		HREF: "https://github.com/karimdaghari",
+	},
+	{
+		NAME: "LinkedIn",
+		HREF: "https://www.linkedin.com/in/karmdaghari",
+	},
 ];
+
+export const RESUME_URL =
+	"https://docs.google.com/document/d/1Tk9TD9NA2JFgQ6k4VXkwY_ADSks7jiRw1AXH0QIirC4/edit?usp=sharing";
